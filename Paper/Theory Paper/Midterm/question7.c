1dc4e332e163c0a7d717b2c5d84c57c0ce1fbdf8
Muskan12350 <66747331+Muskan12350@users.noreply.github.com>
2020-12-02 22:51:56 +0500

    Create question7.c

---
+++ Paper/Theory Paper/Midterm/question7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int num1, num2, num3=2;
+
+    // Assignment Operators: Used to assign values to variables (e.g: =, +=, -=, *=, /=)
+    num1 = 5;
+    num2 = 8;
+    num3+=2;
+
+    // Equality Operator: Compares the equality between two operands
+    if(num1==num2){
+        printf("Numbers are equal");
+    }
+
+    return 0;
+}
