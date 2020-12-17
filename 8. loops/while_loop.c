69de6ee0ca476895d9366ea1248111957c0540cc
Muskan12350 <66747331+Muskan12350@users.noreply.github.com>
2020-12-17 15:19:51 +0500

    Update while_loop.c

--- 8. loops/while_loop.c
+++ 8. loops/while_loop.c
@@ -14,21 +14,21 @@ int main(){
     while(j<=10){
     	printf("%d\n", j);
     	j++;
-	}
+    }
 
     // print numbers from 10 to 1
     int k=10;
     while(k>=1){
     	printf("%d\n", k);
     	k--;
-	}
+    }
 
     // print even numbers from 1 to 10
     int x=2;
     while(x<=10){
     	printf("%d\n", x);
     	x=x+2;
-	}
+    }
 
     // print odd numbers from 1 to 10
 
