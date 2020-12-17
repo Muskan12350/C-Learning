6784c8718fbf18810133182ac52aa4b7f2ab4b57
Muskan12350 <66747331+Muskan12350@users.noreply.github.com>
2020-12-17 14:56:12 +0500

    Add files via upload

--- 8. loops/while_loop.c
+++ 8. loops/while_loop.c
@@ -10,12 +10,34 @@ int main(){
     }
 
     // print numbers from 1 to 10
+    int j=1;
+    while(j<=10){
+    	printf("%d\n", j);
+    	j++;
+	}
 
     // print numbers from 10 to 1
+    int k=10;
+    while(k>=1){
+    	printf("%d\n", k);
+    	k--;
+	}
 
     // print even numbers from 1 to 10
+    int x=2;
+    while(x<=10){
+    	printf("%d\n", x);
+    	x=x+2;
+	}
 
     // print odd numbers from 1 to 10
-
+    
+int y=1;
+    while(y<=10){
+    	printf("%d\n", y);
+    	y=y+2;
+    }
+    	
     return 0;
 }
+
