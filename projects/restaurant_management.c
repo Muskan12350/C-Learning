c2349865c557f6caf223d1e5d28a27a6b330d7da
Muskan12350 <66747331+Muskan12350@users.noreply.github.com>
2020-12-23 19:50:32 +0500

    Add files via upload

--- projects/restaurant_management.c
+++ projects/restaurant_management.c
@@ -54,9 +54,32 @@ void showMenuCard(){
 	printf("\n");
 }
 
+// function to take quantity input and returns the price for each item
+int getItemBill(char itemName[], int itemPrice){
+    printf("---------------------------------------------------------------------------\n");
+
+    // Take quantity input
+    int quantity;
+    printf("\tEnter quantity: ");
+    scanf("%d", &quantity);
+
+    printf("\tItem Name = %s\n", itemName);
+
+    int itemBill = itemPrice * quantity;
+    printf("\tBill =  %d\n", itemBill);
+
+    printf("---------------------------------------------------------------------------\n\n");
+
+    return itemBill;
+}
+
 int main(){
     // Required Variables
-    int orderChoice;
+    int orderChoice, totalBill=0;
+    int item1Bill=0, item2Bill=0, item3Bill=0, item4Bill=0;
+    int item5Bill=0, item6Bill=0, item7Bill=0, item8Bill=0;
+    int item9Bill=0, item10Bill=0, item11Bill=0, item12Bill=0;
+    int item13Bill=0, item14Bill=0, item15Bill=0, item16Bill=0;
 
     // Calling showDeveloperDetail() to show the developer details
     showDeveloperDetail();
@@ -72,8 +95,41 @@ int main(){
 
         // Check user selected choice using switch case
         switch(orderChoice){
-            case 0: exit(0);
-                    break;
+            case 0:
+                exit(0);
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+            case 16:
+                break;
             default: printf("-----------------------------------------------------------------\n\n");
                      printf("\tInvalid Choice.\n\n");
                      printf("-----------------------------------------------------------------\n");
